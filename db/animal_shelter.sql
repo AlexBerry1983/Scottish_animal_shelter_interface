3d90042646cee5a771fecafab2a6fe965817b923
Alex Berry <alexander.e.berry@hotmail.co.uk>
2017-05-04 16:55:04 +0100

    init commit sql tables

---
+++ db/animal_shelter.sql
@@ -0,0 +1,17 @@
+DROP TABLE IF EXISTS pets;
+DROP TABLE IF EXISTS owners;
+
+CREATE TABLE owners (
+  id: serial4 PRIMARY KEY,
+  name: VARCHAR(255),
+  address: VARCHAR(255)
+);
+
+CREATE TABLE pets (
+  id: serial4 PRIMARY KEY,
+  name: VARCHAR(255),
+  type: VARCHAR(255),
+  admission_date: DATE,
+  adoptable: BOOLEAN,
+  owner_id: INT4 REFERENCES owners(id) ON DELETE CASCADE
+);
