087fa54db025311d0cb5b225c15c62dd3a42a8ba
Alex Berry <alexander.e.berry@hotmail.co.uk>
2017-05-05 15:15:11 +0100

    owner seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,13 +3,34 @@ require('pry-byebug')
  require_relative('../models/pets')
 
  Owner.delete_all()
- 
+ Pet.delete_all()
 
  owner1 = Owner.new({
    'name' => 'Angus',
    'address' => '23 made up street'
  })
 
- owner1.save
+ owner2 = Owner.new({
+   'name' => 'Paul',
+   'address' => '42 imagination drive'
+ })
 
- 
+ owner1 = Owner.new({
+   'name' => 'Leon',
+   'address' => '31 cant be serious close'
+ })
+
+  owner1.save
+  owner2.save
+  owner3.save 
+
+ pet1 = Pet.new({
+   'name' => 'Scrappy',
+   'type' => 'Cat',
+   'admission_date' => 'March 21 2013',
+   'adoptable' => true,
+   'owner_id' => owner1.id
+ })
+
+
+ pet1.save
